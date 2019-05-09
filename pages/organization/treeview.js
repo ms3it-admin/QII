dfb305c3c22323a4d93f5b829fe6babd832c12d4
barghya <barghya27@gmail.com>
2019-05-09 13:25:09 +0530

    adding colour and tree menu

---
+++ pages/organization/treeview.js
@@ -0,0 +1,21 @@
+var tree = [{
+    text: "company1",
+    color: "#f44141",
+    href: "",
+    nodes: [{
+        text: "subsidiary1",
+        color: "#f44141",
+        href: "",
+        nodes: [{
+            text: "division1",
+            color: "#3836bc",
+            href: "",
+            nodes: [{
+                text: "subdivision1",
+                color: "#3836bc",
+                href: "",
+                nodes: []
+            }]
+        }]
+    }]
+}]
