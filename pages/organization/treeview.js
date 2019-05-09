ab1673dc48398706fa3ba41eceba9a86ac51c95a
barghya <barghya27@gmail.com>
2019-05-09 17:59:03 +0530

    modified

--- pages/organization/treeview.js
+++ pages/organization/treeview.js
@@ -1,21 +1,43 @@
 var tree = [{
-    text: "company1",
-    color: "#f44141",
-    href: "",
-    nodes: [{
-        text: "subsidiary1",
-        color: "#f44141",
+        text: "company1",
+        color: "#3836bc",
         href: "",
         nodes: [{
-            text: "division1",
+            text: "subsidiary1",
             color: "#3836bc",
             href: "",
             nodes: [{
-                text: "subdivision1",
-                color: "#3836bc",
+                text: "division1",
+                color: "#f44141",
                 href: "",
-                nodes: []
+                nodes: [{
+                    text: "subdivision1",
+                    color: "#f44141",
+                    href: "",
+                    nodes: []
+                }]
             }]
         }]
-    }]
-}]
+    },
+    {
+        text: "company2",
+        color: "#3836bc",
+        href: "",
+        nodes: [{
+            text: "subsidiary2",
+            color: "#3836bc",
+            href: "",
+            nodes: [{
+                text: "division2",
+                color: "#f44141",
+                href: "",
+                nodes: [{
+                    text: "subdivision2",
+                    color: "#f44141",
+                    href: "",
+                    nodes: []
+                }]
+            }]
+        }]
+    }
+]
