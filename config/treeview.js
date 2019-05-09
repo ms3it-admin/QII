469ce408cb2b3fe11d2295b151a5196f9f7038a6
barghya <barghya27@gmail.com>
2019-05-09 13:23:16 +0530

    adding tree children grandchildren

---
+++ config/treeview.js
@@ -0,0 +1,85 @@
+var tree = [{
+        //company1
+        text: "company1",
+        //name: "company1",
+        href: "",
+        nodes: [{
+                //subsidiary1
+                text: "subsidiary1",
+                //name: "subsidiary1",
+                href: "",
+                nodes: []
+            },
+            {
+                //subsidiary2
+                text: "subsidiary2",
+                //name: "subsidiary2",
+                href: "",
+                nodes: []
+            },
+            {
+                //subsidiary3
+                text: "subsidiary3",
+                //name: "subsidiary3",
+                href: "",
+                nodes: []
+            }
+        ],
+    },
+    {
+        //company2
+        text: "company2",
+        //name: "company2",
+        href: "",
+        nodes: [{
+                //subsidiary1
+                text: "subsidiary1",
+                //name: "subsidiary1",
+                href: "",
+                nodes: []
+            },
+            {
+                //subsidiary2
+                text: "subsidiary2",
+                //name: "subsidiary2",
+                href: "",
+                nodes: []
+            },
+            {
+                //subsidiary3
+                text: "subsidiary3",
+                //name: "subsidiary3",
+                href: "",
+                nodes: []
+            }
+        ],
+    },
+    {
+        //company3
+        text: "company3",
+        //name: "company3",
+        href: "",
+        nodes: [{
+                //subsidiary1
+                text: "subsidiary1",
+                //name: "subsidiary1",
+                href: "",
+                nodes: []
+            },
+            {
+                //subsidiary2
+                text: "subsidiary2",
+                //name: "subsidiary2",
+                href: "",
+                nodes: []
+            },
+            {
+                //subsidiary3
+                text: "subsidiary3",
+                //name: "subsidiary3",
+                href: "",
+                nodes: []
+            }
+        ],
+    },
+];
