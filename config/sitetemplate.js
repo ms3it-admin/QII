f086028b81f543b1ffff5b80d616492eb551eeb6
Sourav Mondal <souravcristiano502@gmail.com>
2019-05-10 16:10:42 +0530

    Adding sitetemplate.js sitetemplate.js, using this file we can populate site template field of this page.

---
+++ config/sitetemplate.js
@@ -0,0 +1,22 @@
+var site = [
+    {
+        id:"1",
+        name:"Shopping Complex"
+    },
+    {
+        id:"2",
+        name:"Residential Apartment"
+    },
+    {
+        id:"3",
+        name:"School Building"
+    },
+    {
+        id:"4",
+        name:"Manufacturing Plant"
+    },
+    {
+        id:"5",
+        name:"Solar Unit"
+    }
+]
