8c9d10fc54ad11dbaa744845a152424fa93cf588
Sourav Mondal <souravcristiano502@gmail.com>
2019-05-10 15:03:34 +0530

    Adding option.js This file contails array of js objects, using those we can populate options for select tag.

---
+++ config/option.js
@@ -0,0 +1,18 @@
+var option = [
+    {
+        id:"1",
+        name:"Select your option"
+    },
+    {
+        id:"2",
+        name:"option1"
+    },
+    {
+        id:"3",
+        name:"option2"
+    },
+    {
+        id:"4",
+        name:"option3"
+    }
+]
