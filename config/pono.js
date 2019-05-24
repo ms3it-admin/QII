06a2b39f8bcdcbe69d0a68e3626416076dcd9f7d
shantanu1998a <shantanurong44@gmail.com>
2019-05-24 18:26:30 +0530

    add json

---
+++ config/pono.js
@@ -0,0 +1,7 @@
+var pon=[
+    {id:"1",name:"Select PO number"},
+    {id:"2",name:"PO_001"},
+    {id:"3",name:"PO_002"},
+    {id:"4",name:"PO_003"},
+    {id:"5",name:"PO_004"},
+];
---
+++ config/storeloc.js
@@ -0,0 +1,7 @@
+var store=[
+    {id:"1",name:"Select a Store Location"},
+    {id:"2",name:"Mumbai"},
+    {id:"3",name:"Delhi"},
+    {id:"4",name:"Kolkata"},
+    {id:"5",name:"Chennai"},
+];
