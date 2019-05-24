78c05f735cb875d4a7c10c4c305e103e948197b9
shantanu1998a <shantanurong44@gmail.com>
2019-05-24 13:49:32 +0530

    add json in nodal in purchaserequisition.html

---
+++ config/material_name.js
@@ -0,0 +1,18 @@
+var material = [
+    {
+        id:"1",
+        name:"Select Your Material"
+    },
+    {
+        id:"2",
+        name:"Sand"
+    },
+    {
+        id:"3",
+        name:"Iron Rod"
+    },
+    {
+        id:"4",
+        name:"Cement"
+    },
+];
