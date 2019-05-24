8a1faeeae33594bcae78e9521d36ebb5b3d53ba0
barghya <32652167+barghya@users.noreply.github.com>
2019-05-24 20:42:40 +0530

    add materialmaster.js

---
+++ config/materialmaster.js
@@ -0,0 +1,42 @@
+var materialcode = [{
+        id: "1",
+        code: "Choose your Material Code"
+    },
+    {
+        id: "2",
+        code: "Material1"
+    },
+    {
+        id: "3",
+        code: "Material2"
+    },
+    {
+        id: "4",
+        code: "Material3"
+    },
+    {
+        id: "5",
+        code: "Material4"
+    },
+    {
+        id: "6",
+        code: "Material5"
+    },
+];
+var materialtype = [{
+        id: "1",
+        type: "Choose your Material Type"
+    },
+    {
+        id: "2",
+        type: "Cement"
+    },
+    {
+        id: "3",
+        type: "Sand"
+    },
+    {
+        id: "4",
+        type: "Iron Rod"
+    },
+];
