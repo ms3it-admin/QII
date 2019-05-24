1a845caf14324032875ee5f4648ce8ac3ba46a77
shantanu1998a <shantanurong44@gmail.com>
2019-05-24 20:33:45 +0530

    add json in organization

---
+++ config/select_type.js
@@ -0,0 +1,18 @@
+var selectType = [
+    {
+        id:"1",
+        type:"choose address type"
+    },
+    {
+        id:"2",
+        type:"Registered Address"
+    },
+    {
+        id:"3",
+        type:"Office Addess"
+    },
+    {
+        id:"4",
+        type:"Mailing Address"
+    },
+];
