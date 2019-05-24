70b700075cfd82650b2ff84cdba0f5a8252d9af5
barghya <32652167+barghya@users.noreply.github.com>
2019-05-24 20:42:26 +0530

    add assetmaster.js

---
+++ config/assetmaster.js
@@ -0,0 +1,42 @@
+var assetCode = [{
+        id: "1",
+        code: "Choose your Asset Code"
+    },
+    {
+        id: "2",
+        code: "ASSET1"
+    },
+    {
+        id: "3",
+        code: "ASSET2"
+    },
+    {
+        id: "4",
+        code: "ASSET3"
+    },
+    {
+        id: "5",
+        code: "ASSET4"
+    },
+    {
+        id: "6",
+        code: "ASSET5"
+    },
+];
+var assettype = [{
+        id: "1",
+        type: "Choose your Asset Type"
+    },
+    {
+        id: "2",
+        type: "JCB"
+    },
+    {
+        id: "3",
+        type: "Utensils"
+    },
+    {
+        id: "4",
+        type: "Truck"
+    },
+];
