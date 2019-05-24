4f4d84d812879cb9eabebbe04bf71f23b0a7f24a
shantanu1998a <shantanurong44@gmail.com>
2019-05-24 17:48:47 +0530

    vendor jsons added

---
+++ config/vendor_names.js
@@ -0,0 +1,26 @@
+var vendoe = [
+	{
+		id:"1",
+		name:"Select Vendor"
+	},
+	{
+		id:"2",
+		name:"UltraTech"
+	},
+	{
+		id:"3",
+		name:"ACC"
+	},
+	{
+		id:"4",
+		name:"Ambuja"
+	},
+	{
+		id:"5",
+		name: "Mahabir Sands"
+	},
+	{
+		id: "6",
+		name: "Garnet Sands"
+	}
+];
