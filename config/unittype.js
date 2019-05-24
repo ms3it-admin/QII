4fa8301cd85c2a1f5dcbe6eb303bed2ff9404057
barghya <32652167+barghya@users.noreply.github.com>
2019-05-24 20:42:55 +0530

    create unittype.js

---
+++ config/unittype.js
@@ -0,0 +1,17 @@
+var unittype = [{
+        id: "1",
+        type: "Choose your Unit Type"
+    },
+    {
+        id: "2",
+        type: "sqft"
+    },
+    {
+        id: "3",
+        type: "kg"
+    },
+    {
+        id: "4",
+        type: "bags"
+    },
+];
