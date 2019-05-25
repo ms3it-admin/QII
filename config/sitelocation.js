cfa43163628bdd9c8b47553d96531e9bbdf9adeb
barghya <32652167+barghya@users.noreply.github.com>
2019-05-25 10:40:46 +0530

    create sitelocation.js

---
+++ config/sitelocation.js
@@ -0,0 +1,17 @@
+var sitelocation = [{
+        id: "1",
+        location: "Choose your Site Location"
+    },
+    {
+        id: "2",
+        location: "Chennai"
+    },
+    {
+        id: "3",
+        location: "Punjab"
+    },
+    {
+        id: "4",
+        location: "Kolkata"
+    },
+];
