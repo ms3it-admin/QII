10c0992e68a05e86479ebb2890f1b66129ff2eb1
barghya <32652167+barghya@users.noreply.github.com>
2019-05-25 10:40:59 +0530

    create storelocation.js

---
+++ config/storelocation.js
@@ -0,0 +1,17 @@
+var storelocation = [{
+        id: "1",
+        location: "Choose your Store Location"
+    },
+    {
+        id: "2",
+        location: "Kolkata"
+    },
+    {
+        id: "3",
+        location: "Mumbai"
+    },
+    {
+        id: "4",
+        location: "Delhi"
+    },
+];
